7e29c962dee5be455c1c4d50e9a5ae6a5d5320bc
Mario Garcia <mariogarciadelvalle@gmail.com>
2026-04-28 13:11:08 +0200

    Dos clases pojo de DailyMotionMiner faltan comment user y caption

---
+++ DailyMotionMiner/src/main/java/aiss/DailyMotionMiner/model/Video.java
@@ -0,0 +1,81 @@
+package aiss.DailyMotionMiner.model;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+
+import com.fasterxml.jackson.annotation.*;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "title",
+        "description",
+        "created_time"
+})
+@Generated("jsonschema2pojo")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Video {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("created_time")
+    private Integer createdTime;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @JsonProperty("description")
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonProperty("description")
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @JsonProperty("created_time")
+    public Integer getCreatedTime() {
+        return createdTime;
+    }
+
+    @JsonProperty("created_time")
+    public void setCreatedTime(Integer createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}

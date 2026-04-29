35c2456983bb1e4d4901ca088263adcc6b3c1d22
Carmen Rodriguez <carrodpue@alum.us.es>
2026-04-29 18:00:42 +0200

    creados model VM

---
+++ DailyMotionMiner/src/main/java/aiss/DailyMotionMiner/model/videominer/VMVideo.java
@@ -0,0 +1,32 @@
+package aiss.DailyMotionMiner.model.videominer;
+
+public class VMVideo {
+
+    private String id;
+    private String name;
+    private String description;
+    private String realeaseTime;
+
+    public VMVideo(String id, String name, String description, String realeaseTime) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.realeaseTime = realeaseTime;
+    }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public String getRealeaseTime() { return realeaseTime; }
+
+    public void setRealeaseTime(String realeaseTime) { this.realeaseTime = realeaseTime; }
+}
---
+++ PeerTubeMiner/src/main/java/aiss/PeerTubeMiner/model/videominer/VMChannel.java
@@ -0,0 +1,39 @@
+package aiss.PeerTubeMiner.model.videominer;
+
+public class VMChannel {
+
+    private String id;
+    private String name;
+    private String description;
+    private String createdTime;
+
+    public VMChannel(String id, String name, String description, String createdTime) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdTime = createdTime;
+    }
+
+    public String getId() { return id; }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreatedTime() { return createdTime; }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+}
